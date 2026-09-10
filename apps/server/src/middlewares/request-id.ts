73fe5df182f9cd1b35c55ee16622222665f66e9d
Xianquan <whoeverimf5@gmail.com>
2026-09-10 22:44:04 +0800

    chore: export a base os

--- apps/server/src/middlewares/request-id.ts
+++ apps/server/src/middlewares/request-id.ts
@@ -1,20 +1,17 @@
 import crypto from "node:crypto";
 
-import { os } from "@orpc/server";
-import type { ResponseHeadersHandlerPluginContext } from "@orpc/server/plugins";
+import { $os } from "@/orpc/os";
 
 export function requestId() {
-  return os
-    .$context<ResponseHeadersHandlerPluginContext>()
-    .middleware(async ({ context, next }) => {
-      const requestId = crypto.randomUUID();
+  return $os.middleware(async ({ context, next }) => {
+    const requestId = crypto.randomUUID();
 
-      context.resHeaders?.set("X-Request-Id", requestId);
+    context.resHeaders?.set("X-Request-Id", requestId);
 
-      return next({
-        context: {
-          requestId,
-        },
-      });
+    return next({
+      context: {
+        requestId,
+      },
     });
+  });
 }
--- apps/server/src/orpc/os.ts
+++ apps/server/src/orpc/os.ts
@@ -1,9 +1,12 @@
 import { implement } from "@orpc/server";
+import { os as __os } from "@orpc/server";
 import type { ResponseHeadersHandlerPluginContext } from "@orpc/server/plugins";
 import { contract } from "@xqvvu/api";
 
 import { requestId } from "@/middlewares/request-id";
 
-export const os = implement(contract)
-  .$context<ResponseHeadersHandlerPluginContext>()
-  .use(requestId());
+export type $Context = ResponseHeadersHandlerPluginContext;
+
+export const $os = __os.$context<$Context>();
+
+export const os = implement(contract).$context<$Context>().use(requestId());
